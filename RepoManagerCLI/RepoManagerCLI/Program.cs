654025ff1e775abe17a20531258398ea5967c8f6
Radek <radek.rogalski88@gmail.com>
2022-06-06 23:27:43 +0200

    Create RepoManagerCLI app

---
+++ RepoManagerCLI/RepoManagerCLI/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace RepoManagerCLI
+{
+    class Options
+    {
+        [Option('t', "token", Required = true, HelpText = "Github personal access token.")]
+        public string Token { get; set; }
+
+        [Option('n', "names", Required = true, HelpText = "List of repositories.", Separator = ',')]
+        public IEnumerable<string> Names { get; set; }
+
+        [Option('d', "delete", Required = false, HelpText = "Weather to delete repositories.")]
+        public bool Delete { get; set; }
+    }
+
+    class Program
+    {
+        async public static Task Main(string[] args)
+        {
+            await Parser.Default.ParseArguments<Options>(args)
+                                .WithParsedAsync(ProcessArguments);
+        }
+
+        async public static Task ProcessArguments(Options options)
+        {
+            RepoManager manager = new(options.Token);
+            HttpResponseMessage response;
+            foreach (string name in options.Names)
+            {
+                response = await (options.Delete ? manager.DeleteRepository(name) : manager.CreateRepository(name));
+                Logger.Target = name;
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.Created:
+                        Logger.Ok("Successfully created repository!");
+                        break;
+
+                    case (HttpStatusCode)422:
+                        Logger.Warning("Repository already exists.");
+                        break;
+
+                    case HttpStatusCode.NoContent:
+                        Logger.Ok("Repository has been deleted.");
+                        break;
+
+                    case HttpStatusCode.NotFound:
+                        Logger.Warning("Repository doesn't exist.");
+                        break;
+
+                    default:
+                        Logger.Fail("An error occured.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ RepoManagerCLI/RepoManagerCLI/RepoManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace RepoManagerCLI
+{
+    class RepoManager
+    {
+        private string Username;
+
+        private HttpClient httpClient = new()
+        {
+            BaseAddress = new Uri("https://api.github.com"),
+            DefaultRequestHeaders =
+            {
+                Accept = { new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json") },
+                UserAgent = { new ProductInfoHeaderValue("RepoManager", "1.0") }
+            }
+        };
+
+        async private Task<string> CheckLogin()
+        {
+            var response = await httpClient.GetAsync("/user");
+            JObject user = JObject.Parse(await response.Content.ReadAsStringAsync());
+            string login = (string)user["login"];
+            return login;
+        }
+
+        public RepoManager(string token)
+        {
+            httpClient.DefaultRequestHeaders
+                .Authorization = new AuthenticationHeaderValue("Token", token);
+            Username = CheckLogin().Result;
+        }
+
+        async public Task<HttpResponseMessage> CreateRepository(string name)
+        {
+            string endpoint = "/user/repos";
+            var payload = new { name };
+            var response = await httpClient.PostAsJsonAsync(endpoint, payload);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> DeleteRepository(string name)
+        {
+            string endpoint = $"/repos/{Username}/{name}";
+            var response = await httpClient.DeleteAsync(endpoint);
+            return response;
+        }
+    }
+}
